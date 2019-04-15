8bb3f729dd56d80bbc0c363a070b138a95395e5c
Denis Cruz Rodrigues <deniscruzrodrigues@gmail.com>
2019-04-15 20:13:46 -0300

    v1

---
+++ src/test/java/com/decruro/project/DecruroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.decruro.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DecruroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
